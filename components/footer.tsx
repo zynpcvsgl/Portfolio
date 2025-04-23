fbe3d86272c25803ff0a136c282a0411cad341fa
ZEYNEP ÇAVUŞOĞLU <94304314+zynpcvsgl@users.noreply.github.com>
2025-04-23 22:25:30 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import React from "react";
+import { useLanguage } from "@/context/language-context";
 
 export default function Footer() {
+  const { language } = useLanguage();
+
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        {language === "tr"
+          ? "© 2025 Zeynep Çavuşoğlu. Tüm hakları saklıdır."
+          : "© 2025 Zeynep Çavuşoğlu. All rights reserved."}
       </small>
       <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        <span className="font-semibold">
+          {language === "tr" ? "Bu web sitesi hakkında:" : "About this website:"}
+        </span>{" "}
+        {language === "tr"
+          ? "React, Next.js, TypeScript, Tailwind CSS, Framer Motion ve Vercel kullanılarak geliştirildi."
+          : "Built with React, Next.js, TypeScript, Tailwind CSS, Framer Motion, and hosted on Vercel."}
       </p>
     </footer>
   );
